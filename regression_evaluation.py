435f5b7301aa0a1c88c67f9a920d4a3a527241aa
YuriiMaisuradze <maisooradze@gmail.com>
2024-06-02 18:32:50 +0200

    Added GradientBosstRegression and restructured folders

---
+++ regression_evaluation.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
+import joblib
+data = pd.read_csv("data/ds_salaries.csv")
+data = data.drop(['salary', 'salary_currency', 'employee_residence', 'company_location'], axis=1)
+label_encoders = {}
+for column in ['work_year', 'experience_level', 'employment_type', 'job_title', 'company_size']:
+    label_encoders[column] = LabelEncoder()
+    data[column] = label_encoders[column].fit_transform(data[column])
+X = data.drop('salary_in_usd', axis=1)
+y = data['salary_in_usd']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+gb_model = GradientBoostingRegressor()
+rf_model = RandomForestRegressor()
+gb_model.fit(X_train_scaled, y_train)
+rf_model.fit(X_train_scaled, y_train)
+gb_predictions = gb_model.predict(X_test_scaled)
+rf_predictions = rf_model.predict(X_test_scaled)
+gb_loss = np.mean((gb_predictions - y_test) ** 2)
+rf_loss = np.mean((rf_predictions - y_test) ** 2)
+gb_mae = np.mean(np.abs(gb_predictions - y_test))
+rf_mae = np.mean(np.abs(rf_predictions - y_test))
+gb_rmse = np.sqrt(np.mean((gb_predictions - y_test) ** 2))
+rf_rmse = np.sqrt(np.mean((rf_predictions - y_test) ** 2))
+
+
+print("Gradient Boosting Metrics:")
+print("Loss:", gb_loss)
+print("MAE:", gb_mae)
+print("RMSE:", gb_rmse)
+
+print("\nRandom Forest Metrics:")
+print("Loss:", rf_loss)
+print("MAE:", rf_mae)
+print("RMSE:", rf_rmse)
+
+# Save models
+joblib.dump(gb_model, 'gb_model.pkl')
+joblib.dump(rf_model, 'rf_model.pkl')
